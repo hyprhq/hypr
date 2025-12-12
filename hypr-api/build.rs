196aa1e444d7bd46ed24f310f869caf5a8fe68c0
ccheshirecat <spuncheshirecat@gmail.com>
2025-12-12 09:14:27 +0700

    Update build.rs

--- hypr-api/build.rs
+++ hypr-api/build.rs
@@ -4,6 +4,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(true)
         .build_client(true)
+        .type_attribute(".", "#[allow(clippy::large_enum_variant)]")
         .compile(&["proto/hypr.proto"], &["proto"])?;
     Ok(())
 }
